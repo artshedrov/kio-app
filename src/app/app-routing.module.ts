734789494fa219b6e1025e5ff82c37a04c250866
artschedrov <art.worknsk@gmail.com>
2019-11-17 12:10:58 +0700

    17.11.19 creat components home section mainlayout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
+import {SectionPageComponent} from './section-page/section-page.component';
+import {MainLayoutComponent} from './public/components/main-layout/main-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: '', component: MainLayoutComponent, children: [
+    {path: '', redirectTo: '/', pathMatch: 'full'},
+    {path: '', component: HomePageComponent},
+    {path: 'section/:id', component: SectionPageComponent}
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SectionPageComponent } from './section-page/section-page.component';
+import { MainLayoutComponent } from './public/components/main-layout/main-layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SectionPageComponent,
+    MainLayoutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
